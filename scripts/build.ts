6905b03ec749a0643f0e57472c757202748816c8
thibaut severac <thib3113@gmail.com>
2020-05-05 10:48:39 +0200

    improve build

--- scripts/build.ts
+++ scripts/build.ts
@@ -5,13 +5,13 @@ import packageJson from '../package.json'
 
 const scssFilename = path.join(__dirname, '..', 'src', 'style.scss');
 
-const cssOutput = path.join(__dirname, '..', 'build', 'style.css');
+const cssOutputFolder = path.join(__dirname, '..', 'build');
 
 const indentWidth = 2;
 
 const options: Options = {
     file: scssFilename,
-    outFile: cssOutput,
+    outFile: path.join(cssOutputFolder, 'style.css'),
     outputStyle: 'expanded',
     indentType: 'space',
     indentWidth
@@ -27,7 +27,7 @@ const urlForum = "https://www.lesimprimantes3d.fr/forum";
 
 
 //check if css_output folder exist
-fs.mkdirSync(path.dirname(cssOutput), { recursive: true });
+fs.mkdirSync(cssOutputFolder, { recursive: true });
 
 sass.render(options, (err, result) => { 
     if(err) {
@@ -43,15 +43,16 @@ ${result.css.toString().split('\n').join(`\n${''.padStart(indentWidth)}`)}
 `
         const CSSChrome = 
 `${CSSHeader}
-@-moz-document url-prefix("${urlForum}") {
 ${result.css.toString().split('\n').join(`\n${''.padStart(indentWidth)}`)}
-}
 `
 
         //generate chrome, firefox and usertyle.css
-        fs.writeFileSync(path.join(path.dirname(cssOutput), 'chrome.css'), CSSChrome);
-        fs.writeFileSync(path.join(path.dirname(cssOutput), 'firefox.css'), CSSUsertyle);
-        fs.writeFileSync(path.join(path.dirname(cssOutput), 'userstyle.css'), CSSUsertyle);
+        fs.writeFileSync(path.join(cssOutputFolder, 'chrome.css'), CSSChrome);
+        fs.writeFileSync(path.join(cssOutputFolder, 'firefox.css'), CSSUsertyle);
+        fs.writeFileSync(path.join(cssOutputFolder, 'userstyle.css'), CSSUsertyle);
+
+        console.log(`css builded in folder ${cssOutputFolder}`);
+
     } else {
         throw new Error('no css generated');
     }
